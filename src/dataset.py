f3292ce6d5aa74329e8d3eb324a7693f5258002d
Andrea Mitta <andrea.mitta@nttdata.com>
2024-09-06 10:01:10 +0200

    Finished cleaning dataset, out.txt to be removed/renamed

--- src/dataset.py
+++ src/dataset.py
@@ -1,42 +1,43 @@
 import pandas as pd
 
-df = pd.read_csv("stretch_exercise_dataset.csv")
+def get_dataset():
 
-columns = list(df.columns)
+    df = pd.read_csv("stretch_exercise_dataset.csv")
 
-dataset = {}
+    columns = list(df.columns)
 
-for i in range(len(df.index)):
-    dataset.update({df.iloc[i, 0]: {key: str(value) for key, value in zip(columns[1:], df.iloc[i, 1:])}})
+    dataset = {}
 
-for key in dataset:
-    if dataset[key]['Preparation'] == '0':
-        dataset[key]['Preparation'] = 'No preparation available'
-        
-    if dataset[key]['Execution'] == '0':
-        dataset[key]['Execution'] = 'No execution available'
+    for i in range(len(df.index)):
+        dataset.update({df.iloc[i, 0]: {key: str(value) for key, value in zip(columns[1:], df.iloc[i, 1:])}})
 
-    target_muscles_list = dataset[key]['Target_Muscles']
-    synergist_muscles_list = dataset[key]['Synergist_Muscles']
+    for key in dataset:
+        if dataset[key]['Preparation'] == '0':
+            dataset[key]['Preparation'] = 'No preparation available'
+            
+        if dataset[key]['Execution'] == '0':
+            dataset[key]['Execution'] = 'No execution available'
 
-    target_muscles_list = dataset[key]['Target_Muscles'].split(',')
-    synergist_muscles_list = dataset[key]['Synergist_Muscles'].split(',')
+        target_muscles_list = dataset[key]['Target_Muscles']
+        synergist_muscles_list = dataset[key]['Synergist_Muscles']
 
-    if target_muscles_list != [''] and target_muscles_list != ['0']:
-        target_muscles_list = [elem.strip() for elem in target_muscles_list if elem != '']
-        target_muscles_list = ', '.join(target_muscles_list)
-    else:
-        target_muscles_list = 'None__'
+        target_muscles_list = dataset[key]['Target_Muscles'].split(',')
+        synergist_muscles_list = dataset[key]['Synergist_Muscles'].split(',')
 
-    if synergist_muscles_list != [''] and synergist_muscles_list != ['0']:
-        synergist_muscles_list = [elem.strip() for elem in synergist_muscles_list if elem != '']
-        synergist_muscles_list = ', '.join(synergist_muscles_list)
-    else:
-        synergist_muscles_list = 'None__'
+        if target_muscles_list != [''] and target_muscles_list != ['0']:
+            target_muscles_list = [elem.strip() for elem in target_muscles_list if elem != '']
+            target_muscles_list = ', '.join(target_muscles_list)
+        else:
+            target_muscles_list = 'None__'
 
-    dataset[key]['Target_Muscles'] = target_muscles_list[:-2]
+        if synergist_muscles_list != [''] and synergist_muscles_list != ['0']:
+            synergist_muscles_list = [elem.strip() for elem in synergist_muscles_list if elem != '']
+            synergist_muscles_list = ', '.join(synergist_muscles_list)
+        else:
+            synergist_muscles_list = 'None__'
 
-    dataset[key]['Synergist_Muscles'] = synergist_muscles_list[:-2]
+        dataset[key]['Target_Muscles'] = target_muscles_list[:-2]
 
+        dataset[key]['Synergist_Muscles'] = synergist_muscles_list[:-2]
 
-print(dataset)
+    return dataset
